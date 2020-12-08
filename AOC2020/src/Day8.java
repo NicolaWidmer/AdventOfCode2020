2b9c7581036f3bfee3b7ae577530ad6dce259835
Nicola <Nicola@192.168.0.178>
2020-12-08 12:29:20 +0100

    Day 8

--- AOC2020/src/Day8.java
+++ AOC2020/src/Day8.java
@@ -3,9 +3,11 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Day8{
+	
 	public static void main(String[] args) {
 		System.out.println(sol());
 	}
+	
 	public static int sol() {
 		File file=new File("Files/8.txt");
 		Scanner sc;
@@ -16,7 +18,77 @@ public class Day8{
 			System.out.println(e);
 			sc=new Scanner(System.in);
 		}
-		int ans=0;
-		return ans;
+		Device device=new Device(sc);
+		System.out.println(device.comp1());
+		return device.comp2();
+	}
+	
+	static class Device{
+		private int pc;
+		private int n; //number of instructions
+		private List<String> instructions;
+		private List<Integer> values;
+		
+		private boolean[] executed;
+		
+		private boolean hasInfinitLoop;
+		
+		Device(Scanner sc){
+			instructions=new ArrayList<String>();
+			values=new ArrayList<Integer>();
+			while(sc.hasNext()) {
+				instructions.add(sc.next());
+				values.add(sc.nextInt());
+			}
+			n=instructions.size();
+		}
+		
+		private int compute() {
+			hasInfinitLoop=false;
+			executed=new boolean[n];
+			pc=0;
+			int ans=0;
+			
+			while(!executed[pc]) {
+				executed[pc]=true;
+				
+				if(instructions.get(pc).equals("jmp"))
+					pc+=values.get(pc);
+				else if(instructions.get(pc).equals("acc")) {
+					ans+=values.get(pc);
+					pc++; }
+				else 
+				  pc++;
+				if(pc>=n) 
+				  return ans;
+			}
+			hasInfinitLoop=true;
+			return ans;
+		}
+		
+		int comp1() {
+			return compute();
+		}
+		
+		int comp2() {
+			int ans=0;
+			for(int i=0;i<instructions.size();i++) {
+				swap(i);
+				ans=compute();
+				swap(i);
+				if(!hasInfinitLoop)break;
+			}
+			return ans;
+			
+		}
+		
+		private void swap(int i) {
+			if(instructions.get(i).equals("jmp")) {
+				instructions.set(i, "nop");
+			}
+			else if(instructions.get(i).equals("nop")) {
+				instructions.set(i, "jmp");
+			}
+		}
 	}
 } 
--- AOC2020/src/GenerateDays.java
+++ AOC2020/src/GenerateDays.java
@@ -1,61 +1,61 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Scanner;
-
-public class GenerateDays {
-	public static void main(String[] args) {
-		for(int i=9;i<26;i++) {
-			day(i);
-		}
-	}
-	static void day(int i) {
-		File f=new File("Files/"+i+".txt");
-		try {
-			f.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		File java=new File("src/Day"+i+".java");
-		try {
-			java.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			OutputStream stream =new FileOutputStream("src/Day"+i+".java");
-			String text="import java.io.File;\n" + 
-					"import java.io.FileNotFoundException;\n" + 
-					"import java.util.*;\n" + 
-					"\n" + 
-					"public class Day"+i+"{\n" + 
-					"	public static void main(String[] args) {\n" + 
-					"		System.out.println(sol());\n" + 
-					"	}\n" + 
-					"	public static int sol() {\n" + 
-					"		File file=new File(\"Files/"+i+".txt\");\n" + 
-					"		Scanner sc;\n" + 
-					"		\n" + 
-					"		try {\n" + 
-					"			sc=new Scanner(file);\n" + 
-					"		} catch (FileNotFoundException e) {\n" + 
-					"			System.out.println(e);\n" + 
-					"			sc=new Scanner(System.in);\n" + 
-					"		}\n" + 
-					"		int ans=0;\n" + 
-					"		return ans;\n" + 
-					"	}\n"+
-					"} ";
-			stream.write(text.getBytes());
-			stream.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-		
-	}
-}
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.OutputStream;
+//import java.util.Scanner;
+//
+//public class GenerateDays {
+//	public static void main(String[] args) {
+//		for(int i=9;i<26;i++) {
+//			day(i);
+//		}
+//	}
+//	static void day(int i) {
+//		File f=new File("Files/"+i+".txt");
+//		try {
+//			f.createNewFile();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		File java=new File("src/Day"+i+".java");
+//		try {
+//			java.createNewFile();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		try {
+//			OutputStream stream =new FileOutputStream("src/Day"+i+".java");
+//			String text="import java.io.File;\n" + 
+//					"import java.io.FileNotFoundException;\n" + 
+//					"import java.util.*;\n" + 
+//					"\n" + 
+//					"public class Day"+i+"{\n" + 
+//					"	public static void main(String[] args) {\n" + 
+//					"		System.out.println(sol());\n" + 
+//					"	}\n" + 
+//					"	public static int sol() {\n" + 
+//					"		File file=new File(\"Files/"+i+".txt\");\n" + 
+//					"		Scanner sc;\n" + 
+//					"		\n" + 
+//					"		try {\n" + 
+//					"			sc=new Scanner(file);\n" + 
+//					"		} catch (FileNotFoundException e) {\n" + 
+//					"			System.out.println(e);\n" + 
+//					"			sc=new Scanner(System.in);\n" + 
+//					"		}\n" + 
+//					"		int ans=0;\n" + 
+//					"		return ans;\n" + 
+//					"	}\n"+
+//					"} ";
+//			stream.write(text.getBytes());
+//			stream.close();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} 
+//		
+//	}
+//}
