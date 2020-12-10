e322acc985f5b87534f66b37c197e006369e5bd8
Nicola <Nicola@192.168.0.178>
2020-12-10 14:34:09 +0100

    day 10

--- AOC2020/src/Day10.java
+++ AOC2020/src/Day10.java
@@ -6,7 +6,7 @@ public class Day10{
 	public static void main(String[] args) {
 		System.out.println(sol());
 	}
-	public static int sol() {
+	public static long sol() {
 		File file=new File("Files/10.txt");
 		Scanner sc;
 		
@@ -17,6 +17,29 @@ public class Day10{
 			sc=new Scanner(System.in);
 		}
 		int ans=0;
-		return ans;
+		List<Integer> list=new ArrayList<Integer>();
+		while(sc.hasNext()) {
+			list.add(sc.nextInt());
+		}
+		int[] arr=new int[list.size()];
+		for(int i=0;i<list.size();i++) {
+			arr[i]=list.get(i);
+		}
+		Arrays.sort(arr);
+		
+		int device=arr[list.size()-1]+3;
+		long[] dp=new long[device+1];
+		dp[0]=1L;
+		for(int i=0;i<list.size();i++) {
+			int cur=arr[i];
+			dp[cur]=0;
+			for(int j=1;j<4&&cur-j>=0;j++) {
+				dp[cur]+=dp[cur-j];
+			}
+		}
+		for(int j=1;j<4;j++) {
+			dp[device]+=dp[device-j];
+		}
+		return dp[device];
 	}
 } 
