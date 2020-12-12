aa1f02b903c47e1591ffc57f7349adf805026c2e
Nicola <Nicola@192.168.0.178>
2020-12-12 11:29:13 +0100

    Day 12

--- AOC2020/src/Day12.java
+++ AOC2020/src/Day12.java
@@ -16,7 +16,39 @@ public class Day12{
 			System.out.println(e);
 			sc=new Scanner(System.in);
 		}
-		int ans=0;
-		return ans;
+		int shipNorth=0;
+		int shipEast=0;
+		int wayNorth=1;
+		int wayEast=10;
+		while(sc.hasNext()) {
+			String line=sc.next();
+			char inst=line.charAt(0);
+			int num=Integer.valueOf(line.substring(1));
+			if(inst=='N')wayNorth+=num;
+			else if(inst=='S')wayNorth-=num;
+			else if(inst=='E')wayEast+=num;
+			else if(inst=='W')wayEast-=num;
+			else if(inst=='F') {
+				shipNorth+=num*wayNorth;
+				shipEast+=num*wayEast;
+			}
+			else{
+				if(num==180) {
+					wayNorth=-wayNorth;
+					wayEast=-wayEast;
+				}
+				else if((inst=='L'&&num==90)||(inst=='R'&&num==270)) {
+					int curN=wayNorth;
+					wayNorth=wayEast;
+					wayEast=-curN;
+				}
+				else if((inst=='L'&&num==270)||(inst=='R'&&num==90)) {
+					int curN=wayNorth;
+					wayNorth=-wayEast;
+					wayEast=curN;
+				}
+			}
+		}
+		return Math.abs(shipNorth)+Math.abs(shipEast);
 	}
 } 
