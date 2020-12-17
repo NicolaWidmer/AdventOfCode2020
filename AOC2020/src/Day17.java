8213dedd4a3ac9b2eee6b21fd3fcb12937989536
Nicola <Nicola@192.168.0.178>
2020-12-17 11:58:01 +0100

    Day 17

--- AOC2020/src/Day17.java
+++ AOC2020/src/Day17.java
@@ -16,7 +16,85 @@ public class Day17{
 			System.out.println(e);
 			sc=new Scanner(System.in);
 		}
+		PocketDimension pocket=new PocketDimension(22, 22, 15,15);
+		for(int i=0;sc.hasNext();i++) {
+			String line=sc.nextLine();
+			for(int j=0;j<line.length();j++) {
+				pocket.set(6+i, 6+j, 7,7, line.charAt(j)=='#');
+			}
+		}
+		for(int count=0;count<6;count++) {
+			PocketDimension newpocket=new PocketDimension(22, 22, 15,15);
+			for(int i=1;i<21;i++) {
+				for(int j=1;j<21;j++) {
+					for(int k=1;k<14;k++) {
+						for(int l=1;l<14;l++) {
+							if(pocket.get(i,j,k,l)) {
+								int num=pocket.numOfAktiveNeigh(i, j, k,l);
+								if(!(num==2||num==3)) newpocket.set(i, j, k,l, false);
+								else newpocket.set(i, j, k,l, true);
+							}
+							else {
+								int num=pocket.numOfAktiveNeigh(i, j, k,l);
+								if(num==3)newpocket.set(i, j, k,l, true);
+								else newpocket.set(i, j, k,l, false);
+							}
+						}
+					}
+				}
+			}
+			
+			pocket=newpocket;
+		}
+		return pocket.getAktive();
+	}
+}
+
+
+class PocketDimension{
+	boolean[][][][] aktive;
+	int dimX,dimY,dimZ,dimW;
+	public PocketDimension(int x,int y,int z,int w) {
+		aktive=new boolean[x][y][z][w];
+		dimX=x;
+		dimY=y;
+		dimZ=z;
+		dimW=w;
+	}
+	boolean get(int x,int y,int z,int w) {
+		return aktive[x][y][z][w];
+	}
+	void set(int x,int y,int z,int w,boolean setTo) {
+		aktive[x][y][z][w]=setTo;
+	}
+	int getAktive() {
 		int ans=0;
+		for(int i=0;i<dimX;i++) {
+			for(int j=0;j<dimY;j++) {
+				for(int k=0;k<dimZ;k++) {
+					for(int l=0;l<dimW;l++){
+						if(aktive[i][j][k][l])ans+=1;
+					}
+				}
+			}
+		}
 		return ans;
 	}
-} 
+	int numOfInAktiveNeigh(int x,int y,int z,int w) {
+		return 80-numOfAktiveNeigh(x,y,z,w);
+	}
+	int numOfAktiveNeigh(int x,int y,int z,int w) {
+		int ans=0;
+		for(int i=-1;i<2;i++) {
+			for(int j=-1;j<2;j++) {
+				for(int k=-1;k<2;k++) {
+					for(int l=-1;l<2;l++){
+						if(aktive[x+i][y+j][z+k][w+l])ans+=1;
+					}
+				}
+			}
+		}
+		if(aktive[x][y][z][w])ans-=1;
+		return ans;
+	}
+}
