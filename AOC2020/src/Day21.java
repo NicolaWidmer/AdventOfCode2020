6cef708eea4f1b8cd9f0318fd7a546b9aae4b34e
Nicola <Nicola@192.168.0.178>
2020-12-21 15:10:26 +0100

    Day 21

--- AOC2020/src/Day21.java
+++ AOC2020/src/Day21.java
@@ -17,6 +17,67 @@ public class Day21{
 			sc=new Scanner(System.in);
 		}
 		int ans=0;
+		
+		List<String> all=new ArrayList<String>();
+		Map<String,Set<String>> allergToPosIngr=new HashMap<String,Set<String>>();
+		
+		while(sc.hasNext()) {
+			String line=sc.nextLine();
+			Set<String> cur=new HashSet<String>();
+			Scanner s=new Scanner(line);
+			String next=s.next();
+			while(!next.equals("(contains")) {
+				all.add(next);
+				cur.add(next);
+				next=s.next();
+			}
+			while(s.hasNext()) {
+				String allerg=s.next().replace(",", "").replace(")", "");
+				if(!allergToPosIngr.containsKey(allerg))allergToPosIngr.put(allerg,cur);
+				else {
+					Set<String> pos=allergToPosIngr.get(allerg);
+					Set<String> nextpos=new HashSet<String>();
+					for(String str:pos) {
+						if(cur.contains(str))nextpos.add(str);
+					}
+					allergToPosIngr.put(allerg, nextpos);
+				}
+			}
+		}
+		Map<String,String> Allerg=new TreeMap<String,String>();
+		boolean change=true;
+		while(change) {
+			String next="";
+			String ingr="";
+			for(String str:allergToPosIngr.keySet()) {
+				if(allergToPosIngr.get(str).size()==1) {
+					next=str;
+					ingr=allergToPosIngr.get(str).iterator().next();
+					break;
+				}
+			}
+			change=!next.equals("");
+			if(change) {
+				Allerg.put(next,ingr);
+				allergToPosIngr.remove(next);
+				for(var cur:allergToPosIngr.values()) {
+					cur.remove(ingr);
+				}
+			}
+		}
+		/*
+		 * Part 1
+		 */
+		for(String cur:all) {
+			if(!Allerg.values().contains(cur))ans++;
+		}
+		/*
+		 * Part 2
+		 */
+		for(String cur:Allerg.values()) {
+			System.out.print(cur+",");
+		}
+		System.out.println();
 		return ans;
 	}
 } 
