46323b8d6fa584f25922985f418d560fc1087569
Nicola <Nicola@192.168.0.178>
2020-12-25 11:15:07 +0100

    day 25

--- AOC2020/src/Day25.java
+++ AOC2020/src/Day25.java
@@ -6,7 +6,7 @@ public class Day25{
 	public static void main(String[] args) {
 		System.out.println(sol());
 	}
-	public static int sol() {
+	public static long sol() {
 		File file=new File("Files/25.txt");
 		Scanner sc;
 		
@@ -16,7 +16,23 @@ public class Day25{
 			System.out.println(e);
 			sc=new Scanner(System.in);
 		}
-		int ans=0;
+		long mod=20201227L;
+		long card=sc.nextLong();
+		long door=sc.nextLong();
+		long loopcard=calcLoop(card,mod);
+		long ans=1;
+		for(int i=0;i<loopcard;i++) {
+			ans=(ans*door)%mod;
+		}
+		return ans;
+	}
+	static long calcLoop(long key,long mod) {
+		long ans=0;
+		long cur=1;
+		while(cur!=key) {
+			ans++;
+			cur=(cur*7)%mod;
+		}
 		return ans;
 	}
 } 
